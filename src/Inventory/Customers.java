d33a4ef5f915c3400dc9d8acf9afa029e85be8a1
john <johnedmondosn83@yahoo.co.uk>
2024-10-29 11:21:20 +0000

    Customer and customer classes completed and manually tested

---
+++ src/Inventory/Customers.java
@@ -0,0 +1,65 @@
+package Inventory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Customers {
+    private HashMap<Integer, Customer> customers = new HashMap<Integer, Customer>();
+    public Customers(){};
+    public void create_customer(String firstname, String lastname, String email){
+        Customer newCust = new Customer(firstname, lastname, email);
+        customers.put(newCust.getCustomerId(), newCust);
+    }
+
+    public String display_customer(int customerId){
+        Customer foundCust = customers.get(customerId);
+        if (foundCust != null){ return foundCust.toString();}
+        return "customer not found";
+    }
+
+    public String display_customer(String email) {
+        for (Map.Entry<Integer, Customer> entry : customers.entrySet()) {
+            Customer customer = entry.getValue();
+            if (customer.getEmail().equals(email)) {
+                return customer.toString();
+            }
+        }
+        return "Customer not found";
+    }
+
+    public String updateCustomer(int customerId, String firstname, String lastname, String email){
+            Customer foundCust = customers.get(customerId);
+            if(foundCust != null) {
+                foundCust.setEmail(email);
+                foundCust.setFirstname(firstname);
+                foundCust.setLastname(lastname);
+                return "customer updated";
+            }
+            return "customer not found";
+    }
+
+    public String deleteCustomer(int customerId){
+        Customer deletedCust = customers.remove(customerId);
+        if (deletedCust == null) return "customer not found";
+        return "customer deleted";
+    }
+    public String deleteCustomer(String email){
+        for (Map.Entry<Integer, Customer> entry : customers.entrySet()) {
+            Customer customer = entry.getValue();
+            if (customer.getEmail().equals(email)) {
+                customers.remove(customer.getCustomerId());
+                return "Customer deleted";
+            }
+        }
+        return "Customer not found";
+    }
+
+    public void displayAllCustomer(){
+        for (Map.Entry<Integer, Customer> entry : customers.entrySet()) {
+            Customer customer = entry.getValue();
+            System.out.println(customer.toString());
+        }
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import Inheritance.ElectronicDevice;
 import Inheritance.Laptop;
 import Inheritance.Radio;
+import Inventory.Customer;
+import Inventory.Customers;
 import UniqueSum.UniqueSum;
 import blackjack.BlackJack;
 import taxes.Person;
@@ -13,35 +15,20 @@ import java.time.LocalDate;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-//        // Press Alt+Enter with your caret at the highlighted text to see how
-//        // IntelliJ IDEA suggests fixing it.
-//        Human john = new Human("brown", 10);
-//        Human jacob = new Human("brown", LocalDate.of(2001, 12,07), 10);
-//        Human jeff = new Human();
-//        System.out.println(jacob.getAgeLocal());
-//        System.out.println(john.getAgeLocal());
-//        ElectronicDevice[] electronicDevices = new ElectronicDevice[4];
-//        ElectronicDevice laptop1 = new Laptop(true, "Dell", 8);
-//        ElectronicDevice laptop2 = new Laptop(true, "HP", 16);
-//
-//        ElectronicDevice radio1 = new Radio();
-//        ElectronicDevice radio2 = new Radio(false, "Phillips", new Double[]{99.8, 102.2});
-//
-//
-//        electronicDevices[0] = laptop1;
-//        electronicDevices[1] = laptop2;
-//
-//        electronicDevices[2] = radio1;
-//        electronicDevices[3] = radio2;
-//
-//
-//
-//        for(ElectronicDevice device : electronicDevices) {
-//            device.turnOn();
-//        }
-//
-//        ElectronicDevice test1 = new ElectronicDevice(){};
-//
+        Customers customers = new Customers();
+
+        customers.create_customer("jim", "smith", "jimsmith@gmail.com");
+        customers.create_customer("heater", "wheatley", "heatherwheatley@gmail.com");
+
+
+
+
+        customers.displayAllCustomer();
+
+        System.out.println(customers.deleteCustomer("jimsmith@gmail.com"));
+        System.out.println(customers.deleteCustomer("jimsmith@gmail"));
+
+        customers.displayAllCustomer();
 
 
 }}
